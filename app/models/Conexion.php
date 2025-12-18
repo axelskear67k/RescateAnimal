69f934b540e77d03d712675aecfa4764dea331ae
Jean HT <Alexandro.torres1232@gmail.com>
2025-12-18 16:31:12 -0500

    Construccion clase Conexion

--- app/models/Conexion.php
+++ app/models/Conexion.php
@@ -12,7 +12,18 @@ class Conexion{
 
   public function getConexion(){
     //manejador de excepciones/errores
+    //try (intentar)
+    //ctach (accidente, error)
     try{
+      //La clase PDO Permite conectarte a diferentes motores de BD,
+      //Requiere una configuracion minima y es facil de utilizar
+      $pdo = new PDO(
+        "mysql:host={$this->servidor}; port={$this->puerto}; dbname={$this->baseDatos}; charset=UTF8",
+        $this->usuario,
+        $this->clave);
+      //configurar el manejo de rrores PDO
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $pdo;
       
     }
     catch(Exception $e){
